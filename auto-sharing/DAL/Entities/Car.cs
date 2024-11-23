a3d2ef874f8ce251c40cc7d78befabfc23fcda92
Dime <dmutrokarpenko@gmail.com>
2024-11-23 13:45:39 +0200

    Added entities and enums

---
+++ auto-sharing/DAL/Entities/Car.cs
@@ -0,0 +1,14 @@
+﻿
+using DAL.Enums;
+
+namespace DAL.Entities
+{
+    public class Car
+    {
+        public required int Id { get; set; }
+        public required string LicencePlate { get; set; }
+        public required int Model { get; set; }
+        public required string Year { get; set; }
+        public required CarStatus Status { get; set; }
+    }
+}
---
+++ auto-sharing/DAL/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DAL.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public required string LicenceNumber { get; set; }
+        public required string Password { get; set; }
+    }
+}
