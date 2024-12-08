5e972b4df5062359e5103c4dbb710579a824c9da
Dime <163345642+Dime2552@users.noreply.github.com>
2024-12-08 12:37:36 +0200

    Added Security

---
+++ auto-sharing/CCL/Security/Identity/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCL.Security.Identity
+{
+    public abstract class User
+    {
+        public User(int id, string name, string password, string userType)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+            UserType = userType;
+        }
+        public int Id { get; }
+        public string Name { get; }
+        public string Password { get; }
+        protected string UserType { get; }
+    }
+}
